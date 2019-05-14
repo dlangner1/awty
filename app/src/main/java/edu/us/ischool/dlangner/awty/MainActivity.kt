ae2c765c4dca0b5c30cba58b397bae4e6dc5395e
Dustin Langner <dustin.langner1@gmail.com>
2019-05-14 12:22:30 -0700

    Show toast when app is in background on timer

--- app/src/main/java/edu/us/ischool/dlangner/awty/MainActivity.kt
+++ app/src/main/java/edu/us/ischool/dlangner/awty/MainActivity.kt
@@ -1,14 +1,24 @@
 package edu.us.ischool.dlangner.awty
 
+import android.app.Service
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.IBinder
 import android.telephony.PhoneNumberUtils
 import android.widget.Toast
+import java.util.*
+import kotlin.concurrent.timerTask
 
 private const val CONTACT_FRAGMENT_TAG = "CONTACT_FRAGMENT_CONTACT"
 
 class MainActivity : AppCompatActivity(),
     ContactFragment.OnFragmentInteractionListener {
+
+    lateinit var toast: Toast
+    var timeToResend: Long = 0
+    val timer = Timer()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,12 +36,23 @@ class MainActivity : AppCompatActivity(),
             // create toast message
             val formattedNumber = PhoneNumberUtils.formatNumber(phoneNumber, "1", "US")
             val formattedMessage = formattedNumber.plus(": ").plus(message)
-            val toast = Toast.makeText(this, formattedMessage, Toast.LENGTH_LONG)
+            toast = Toast.makeText(this, formattedMessage, Toast.LENGTH_LONG)
+            timeToResend = (duration * 60000).toLong() // converts minutes to milliseconds
 
-            // start running on timer for background service
+            val handler = Handler()
 
+            timer.scheduleAtFixedRate(timerTask {
+                run {
+                    handler.post {
+                        run {
+                            toast.show()
+                        }
+                    }
+                }
+            }, 0, timeToResend)
         } else {
             // stop service
+            timer.cancel()
         }
     }
 }
